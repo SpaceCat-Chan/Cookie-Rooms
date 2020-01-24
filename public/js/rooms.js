0725ed5806d313b340b558fdd6d3deecf2af6257
rasm685p <47851863+rasm685p@users.noreply.github.com>
2020-01-24 13:45:16 +0100

    Added a 'how to use' section

--- public/js/rooms.js
+++ public/js/rooms.js
@@ -4,7 +4,7 @@ socket.on('server-rooms', (data)=>{
     for (var i in data) {
         data[i].id = `${data[i].id[0].toUpperCase()}${data[i].id.slice(1)}`;
         document.getElementById("box-name").innerHTML += `<p><span>${data[i].id}</span></p>`;
-        document.getElementById("box-worth").innerHTML += `<p><span>${data[i].worth}</span></p>`;
+        document.getElementById("box-worth").innerHTML += `<p><span>$${data[i].worth}</span></p>`;
         document.getElementById("box-online").innerHTML += `<p><span>${data[i].users}</span></p>`;
         document.getElementById("box-action").innerHTML += `<p><button>Join</button></p>`;
     }
